75b22bd3a00b71aeac98bc40d58510f2fecff424
Andrey Milykh <mav_github@mail.ru>
2023-02-27 15:25:28 +0300

    Milykh_Andrey_dz_8 is completed

--- src/Milykh_Andrey_dz_8/task_02.py
+++ src/Milykh_Andrey_dz_8/task_02.py
@@ -17,22 +17,35 @@ from pathlib import Path
 from package_for_serialization import dir2list as dl
 
 
+def show_comparison(message_: str, result_: bool):
+    if result_:
+        print(f"{message_} equal.")
+    else:
+        print(f"{message_} not equal.")
+
+
 if __name__ == '__main__':
     # получаем текущий рабочий каталог
     work_path = os.getcwd()
+
+    # рекурсивно обходим директорию и сохраняем результат в переменной
     res_list = dl.dir2list(work_path)
-    # сохраняем результаты в файлах
+
+    # сохраняем переменную в файлах формата json, csv, pickle
     dl.save2json(res_list, Path('file_in.json'))
     dl.save2csv(res_list, Path('file_in.csv'))
     dl.save2pickle(res_list, Path('file_in.pickle'))
 
+    # загружаем поочерёдно содержимое файлов в тестовую
+    # переменную res_ и сравниваем её с исходной переменной res_list
     pickle_list = dl.load4pickle(Path('file_in.pickle'))
+    res_ = dl.list_comparison(pickle_list, res_list)
+    show_comparison("Lists res_list and pickle_list are", res_)
 
-    res = [x for x in res_list +
-           pickle_list if x not in res_list or x not in pickle_list]
+    csv_list = dl.load4csv(Path('file_in.csv'))
+    res_ = dl.list_comparison(csv_list, res_list)
+    show_comparison("Lists res_list and csv_list are", res_)
 
-    # print(res)
-    if not res:
-        print("Lists res_list and pickle_list are equal")
-    else:
-        print("Lists res_list and pickle_list are not equal")
+    json_list = dl.load4json(Path('file_in.json'))
+    res_ = dl.list_comparison(csv_list, res_list)
+    show_comparison("Lists res_list and csv_list are", res_)
